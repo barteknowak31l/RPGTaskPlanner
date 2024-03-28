da9165c9c0be014503028d20d4c957c75aa51638
Bartek Nowak <barteknowak31l@gmail.com>
2024-03-28 11:58:21 +0100

    add add task activity

--- app/src/main/java/edu/put/rpgtaskplanner/task_list/TaskListActivity.kt
+++ app/src/main/java/edu/put/rpgtaskplanner/task_list/TaskListActivity.kt
@@ -2,7 +2,6 @@ package edu.put.rpgtaskplanner.task_list
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import edu.put.rpgtaskplanner.databinding.ActivityTaskListBinding
 
@@ -15,6 +14,11 @@ class TaskListActivity : AppCompatActivity(), TaskListFragment.Listener {
 
         binding = ActivityTaskListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCreate.setOnClickListener{
+            val intent = Intent(this, AddTaskActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
